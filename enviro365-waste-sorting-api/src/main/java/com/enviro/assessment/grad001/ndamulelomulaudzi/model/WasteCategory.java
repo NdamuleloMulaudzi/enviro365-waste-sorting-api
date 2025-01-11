d2d58faa3ddb5587ad376a1ae3af76987aa11fb0
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2025-01-11 18:05:57 +0200

    add WasteCategory entity with validation annotations

---
+++ enviro365-waste-sorting-api/src/main/java/com/enviro/assessment/grad001/ndamulelomulaudzi/model/WasteCategory.java
@@ -0,0 +1,45 @@
+package com.enviro.assessment.grad001.ndamulelomulaudzi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class WasteCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+   //getters
+   public String getName() {
+       return name;
+   }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+}
