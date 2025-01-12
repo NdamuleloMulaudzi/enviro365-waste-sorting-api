03a5529f9a53b0d0b54d34b33bc0a8afe5463e13
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2025-01-12 09:35:26 +0200

    feat: add DisposalGuidelineRepository interface for database interactions - Extends JpaRepository to provide CRUD operations for DisposalGuideline

---
+++ enviro365-waste-sorting-api/src/main/java/com/enviro/assessment/grad001/ndamulelomulaudzi/repository/DisposalGuidelineRepository.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.ndamulelomulaudzi.repository;
+
+import com.enviro.assessment.grad001.ndamulelomulaudzi.model.DisposalGuideline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuideline, Long> {
+}
