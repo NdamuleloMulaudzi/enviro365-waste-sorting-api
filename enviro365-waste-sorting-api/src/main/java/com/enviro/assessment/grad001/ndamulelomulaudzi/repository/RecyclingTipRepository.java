15b9338fc36790c22320ab8bc2e763908d3d4ce9
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2025-01-12 10:31:51 +0200

    create RecyclingTipRepository for database operations

---
+++ enviro365-waste-sorting-api/src/main/java/com/enviro/assessment/grad001/ndamulelomulaudzi/repository/RecyclingTipRepository.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.ndamulelomulaudzi.repository;
+
+import com.enviro.assessment.grad001.ndamulelomulaudzi.model.RecyclingTip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecyclingTipRepository extends JpaRepository<RecyclingTip, Long> {
+}
