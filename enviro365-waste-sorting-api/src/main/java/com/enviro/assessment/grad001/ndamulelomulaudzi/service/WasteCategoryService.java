c8e14081a818a655becd5943706f197bb7b36752
NdamuleloMulaudzi <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2025-01-13 13:06:39 +0200

    Added Service implementation

--- enviro365-waste-sorting-api/src/main/java/com/enviro/assessment/grad001/ndamulelomulaudzi/service/WasteCategoryService.java
+++ enviro365-waste-sorting-api/src/main/java/com/enviro/assessment/grad001/ndamulelomulaudzi/service/WasteCategoryService.java
@@ -1,37 +1,13 @@
 package com.enviro.assessment.grad001.ndamulelomulaudzi.service;
 
 import com.enviro.assessment.grad001.ndamulelomulaudzi.model.WasteCategory;
-import com.enviro.assessment.grad001.ndamulelomulaudzi.repository.WasteCategoryRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class WasteCategoryService {
-    private final WasteCategoryRepository repository;
-
-    public WasteCategoryService(WasteCategoryRepository repository) {
-        this.repository = repository;
-    }
-
-    // Fetch all waste category from the database.
-    public List<WasteCategory> getAllCategories() {
-        return repository.findAll();
-    }
-
-    // Fetch a specific category by ID
-    public Optional<WasteCategory> getCategoryById(Long id) {
-        return repository.findById(id);
-    }
-
-    // Save a category or update an existing one.
-    public WasteCategory saveCategory(WasteCategory category) {
-        return repository.save(category);
-    }
-
-    // Delete a category by ID
-    public void deleteCategory(Long id) {
-        repository.deleteById(id);
-    }
+public interface WasteCategoryService {
+    List<WasteCategory> getAllCategories();
+    Optional<WasteCategory> getCategoryById(Long id);
+    WasteCategory saveCategory(WasteCategory category);
+    void deleteCategory(Long id);
 }
